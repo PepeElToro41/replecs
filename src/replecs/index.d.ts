30bbd2806967546d38d41649379c75c7ca24df15
PepeElToro41 <da907852@gmail.com>
2025-07-26 12:50:31 -0600

    add replecs lib

--- src/replecs/index.d.ts
+++ src/replecs/index.d.ts
@@ -1,87 +1,94 @@
 import type { Entity, Id, Tag, World } from "@rbxts/jecs";
 
 declare namespace Replecs {
-  export interface SerdesTable {
-    serialize: (value: any) => buffer;
-    deserialize: (buffer: buffer) => any;
-  }
+   export interface SerdesTable {
+      serialize: (value: any) => buffer;
+      deserialize: (buffer: buffer) => any;
+   }
 
-  export interface ObserverWorld extends World {
-    added<T>(
-      this: ObserverWorld,
-      component: Id<T>,
-      callback: (e: Entity, id: Id<T>, value?: T) => void,
-    ): () => void;
-    removed<T>(
-      this: ObserverWorld,
-      component: Id<T>,
-      callback: (e: Entity, id: Id<T>) => void,
-    ): () => void;
-    changed<T>(
-      this: ObserverWorld,
-      component: Id<T>,
-      callback: (e: Entity, id: Id<T>, value?: T) => void,
-    ): () => void;
-  }
+   export interface ObserverWorld extends World {
+      added<T>(
+         this: ObserverWorld,
+         component: Id<T>,
+         callback: (e: Entity, id: Id<T>, value?: T) => void
+      ): () => void;
+      removed<T>(
+         this: ObserverWorld,
+         component: Id<T>,
+         callback: (e: Entity, id: Id<T>) => void
+      ): () => void;
+      changed<T>(
+         this: ObserverWorld,
+         component: Id<T>,
+         callback: (e: Entity, id: Id<T>, value?: T) => void
+      ): () => void;
+   }
 
-  type MemberFilter = Map<Player, boolean>;
+   type MemberFilter = Map<Player, boolean>;
 
-  export interface Components {
-    shared: Tag;
-    networked: Entity<MemberFilter | undefined>;
-    reliable: Entity<MemberFilter | undefined>;
-    unreliable: Entity<MemberFilter | undefined>;
-    pair: Tag;
+   export interface Components {
+      shared: Tag;
+      networked: Entity<MemberFilter | undefined>;
+      reliable: Entity<MemberFilter | undefined>;
+      unreliable: Entity<MemberFilter | undefined>;
+      pair: Entity<MemberFilter | undefined>;
 
-    serdes: Entity<SerdesTable>;
-    bytespan: Entity<number>;
-    custom_id: Entity<(value: any) => Entity>;
-    __alive_tracking__: Tag;
+      serdes: Entity<SerdesTable>;
+      bytespan: Entity<number>;
+      custom_id: Entity<((value: any) => Entity) | undefined>;
 
-    Shared: Tag;
-    Networked: Entity<MemberFilter | undefined>;
-    Reliable: Entity<MemberFilter | undefined>;
-    Unreliable: Entity<MemberFilter | undefined>;
-    Pair: Tag;
+      Shared: Tag;
+      Networked: Entity<MemberFilter | undefined>;
+      Reliable: Entity<MemberFilter | undefined>;
+      Unreliable: Entity<MemberFilter | undefined>;
+      Pair: Entity<MemberFilter | undefined>;
 
-    Serdes: Entity<SerdesTable>;
-    Bytespan: Entity<number>;
-    CustomId: Entity<(value: any) => Entity>;
-  }
+      Serdes: Entity<SerdesTable>;
+      Bytespan: Entity<number>;
+      CustomId: Entity<((value: any) => Entity) | undefined>;
+   }
 
-  export interface Client {
-    world: ObserverWorld;
-    inited?: boolean;
+   export interface Client {
+      world: ObserverWorld;
+      inited?: boolean;
 
-    init(world?: ObserverWorld): void;
-    destroy(): void;
-    after_replication(callback: () => void): void;
+      init(world?: ObserverWorld): void;
+      destroy(): void;
+      after_replication(callback: () => void): void;
 
-    apply_updates(buf: buffer, all_variants?: any[][]): void;
-    apply_unreliable(buf: buffer, all_variants?: any[][]): void;
-    apply_full(buf: buffer, all_variants?: any[][]): void;
-  }
+      apply_updates(buf: buffer, all_variants?: any[][]): void;
+      apply_unreliable(buf: buffer, all_variants?: any[][]): void;
+      apply_full(buf: buffer, all_variants?: any[][]): void;
+   }
 
-  export interface Server {
-    world: ObserverWorld;
-    inited?: boolean;
+   export interface Server {
+      world: ObserverWorld;
+      inited?: boolean;
 
-    init(world?: ObserverWorld): void;
-    destroy(): void;
+      init(world?: ObserverWorld): void;
+      destroy(): void;
 
-    get_full(player: Player): LuaTuple<[buffer, any[][]]>;
-    collect_updates(): IterableFunction<LuaTuple<[Player, buffer, any[][]]>>;
-    collect_unreliable(): IterableFunction<LuaTuple<[Player, buffer, any[][]]>>;
-    mark_player_ready(player: Player): void;
-    is_player_ready(player: Player): boolean;
-  }
+      get_full(player: Player): LuaTuple<[buffer, any[][]]>;
+      collect_updates(): IterableFunction<LuaTuple<[Player, buffer, any[][]]>>;
+      collect_unreliable(): IterableFunction<
+         LuaTuple<[Player, buffer, any[][]]>
+      >;
+      mark_player_ready(player: Player): void;
+      is_player_ready(player: Player): boolean;
+   }
 
-  export interface Replecs extends Components {
-    after_replication(world: ObserverWorld): void;
+   export interface ReplecsLib {
+      client: Client;
+      server: Server;
 
-    create_server: (world?: ObserverWorld) => Server;
-    create_client: (world?: ObserverWorld) => Client;
-  }
+      after_replication(world: ObserverWorld): void;
+   }
+
+   export interface Replecs extends Components {
+      create: (world?: ObserverWorld) => ReplecsLib;
+      create_server: (world?: ObserverWorld) => Server;
+      create_client: (world?: ObserverWorld) => Client;
+   }
 }
 
 declare const Replecs: Replecs.Replecs;
